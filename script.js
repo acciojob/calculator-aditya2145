64615969f24ce59bc2cf9a545e9f85b5ee29aa10
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2025-02-22 13:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const inputField = document.getElementById("input");
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        const id = button.id;
+
+        if (id.startsWith("block")) {
+            inputField.value += button.innerText;
+        } else if (id === "dot") {
+            inputField.value += ".";
+        } else if (id === "plus") {
+            inputField.value += "+";
+        } else if (id === "minus") {
+            inputField.value += "-";
+        } else if (id === "multiply") {
+            inputField.value += "*";
+        } else if (id === "divide") {
+            inputField.value += "/";
+        } else if (id === "ans") {
+            try {
+                inputField.value = eval(inputField.value);
+            } catch (error) {
+                inputField.value = "Error";
+            }
+        } else if (id === "clr") {
+            inputField.value = "";
+        }
+    });
+});
